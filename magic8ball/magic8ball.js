fde763d78d6a917be10122116c1cd26cceaf9fc9
Lalo <114935993+lalolel@users.noreply.github.com>
2023-07-22 20:00:12 +0300

    Update magic8ball.js

--- magic8ball/magic8ball.js
+++ magic8ball/magic8ball.js
@@ -1,3 +1,20 @@
+/*
+Variable randomNumber = Math.floor(Math.random() * 8) uses 2 methods from the Math library and generates a random number from 0 to 8.
+
+If the randomNumber is 0, then save an answer to the eightBall variable; if randomNumber is 1, then save the next answer, and so on.
+
+
+
+*/
+
+
+
+let userName = "John";
+userName ? console.log(`Hello, ${userName}!`) : console.log("Hello!");
+let userQuestion = "Will it rain today?";
+console.log(`${userName} asks: ${userQuestion}`);
+let randomNumber = Math.floor(Math.random() * 8);
+let eightBall = "";
 let userName = "John";
 userName ? console.log(`Hello, ${userName}!`) : console.log("Hello!");
 let userQuestion = "Will it rain today?";
@@ -6,33 +23,32 @@ let randomNumber = Math.floor(Math.random() * 8);
 let eightBall = "";
 switch (randomNumber) {
   case 0:
-     eightBall = "It is certain";
+    eightBall = "It is certain";
     break;
   case 1:
-     eightBall = "It is decidedly so";
+    eightBall = "It is decidedly so";
     break;
   case 2:
-     eightBall = "Reply hazy try again";
+    eightBall = "Reply hazy try again";
     break;
   case 3:
-     eightBall = "Cannot predict now";
+    eightBall = "Cannot predict now";
     break;
   case 4:
-     eightBall = "Do not count on it";
+    eightBall = "Do not count on it";
     break;
   case 5:
-     eightBall = "My sources say no";
+    eightBall = "My sources say no";
     break;
   case 6:
-     eightBall = "Outlook not so good";
+    eightBall = "Outlook not so good";
     break;
   case 7:
-     eightBall = "Signs point to yes";
+    eightBall = "Signs point to yes";
     break;
   default:
-     eightBall = "Unknown";
+    eightBall = "Unknown";
     break;
-}
-
+};
 console.log(`The Magic 8 Ball says: ${eightBall}`);
 
