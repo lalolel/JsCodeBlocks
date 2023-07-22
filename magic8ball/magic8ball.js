decaadb87be535df974700d601e148525c81f3dd
Lalo <114935993+lalolel@users.noreply.github.com>
2023-07-22 19:46:22 +0300

    Create magic8ball.js

---
+++ magic8ball/magic8ball.js
@@ -0,0 +1,38 @@
+let userName = "John";
+userName ? console.log(`Hello, ${userName}!`) : console.log("Hello!");
+let userQuestion = "Will it rain today?";
+console.log(`${userName} asks: ${userQuestion}`);
+let randomNumber = Math.floor(Math.random() * 8);
+let eightBall = "";
+switch (randomNumber) {
+  case 0:
+     eightBall = "It is certain";
+    break;
+  case 1:
+     eightBall = "It is decidedly so";
+    break;
+  case 2:
+     eightBall = "Reply hazy try again";
+    break;
+  case 3:
+     eightBall = "Cannot predict now";
+    break;
+  case 4:
+     eightBall = "Do not count on it";
+    break;
+  case 5:
+     eightBall = "My sources say no";
+    break;
+  case 6:
+     eightBall = "Outlook not so good";
+    break;
+  case 7:
+     eightBall = "Signs point to yes";
+    break;
+  default:
+     eightBall = "Unknown";
+    break;
+}
+
+console.log(`The Magic 8 Ball says: ${eightBall}`);
+
