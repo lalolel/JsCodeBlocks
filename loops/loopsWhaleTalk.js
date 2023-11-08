7bad51a16d3728dec8616a3c62917286ae71cf47
Lalo <114935993+lalolel@users.noreply.github.com>
2023-11-08 16:30:34 +0200

    Create loopsWhaleTalk.js
    nested loops

---
+++ loops/loopsWhaleTalk.js
@@ -0,0 +1,26 @@
+let input = "turpentine and turtles";
+const vowels = ["a", "e", "i", "o", "u"];
+const resultArray = [];
+
+for (let inputIndex = 0; inputIndex < input.length; inputIndex++) {
+  // console.log(`inputIndex is ${inputIndex}.`)
+  if (input[inputIndex] === "e") {
+    resultArray.push(input[inputIndex]);
+  }
+  if (input[inputIndex] === "u") {
+    resultArray.push(input[inputIndex]);
+  }
+  for (let vowelIndex = 0; vowelIndex < vowels.length; vowelIndex++) {
+    //console.log(`vowelIndex is ${vowelIndex}.`)
+    if (input[inputIndex] === vowels[vowelIndex]) {
+      //      console.log(input[inputIndex]);
+      resultArray.push(input[inputIndex]);
+      //      console.log(resultArray);
+    }
+  }
+}
+
+// console.log(resultArray);
+
+const resultString = resultArray.join("").toUpperCase();
+console.log(resultString);
