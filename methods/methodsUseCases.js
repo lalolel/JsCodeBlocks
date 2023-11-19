022905ab0d73fe35e17718261a37fffdae18f502
Lalo <114935993+lalolel@users.noreply.github.com>
2023-11-19 18:38:18 +0200

    Create methodsUseCases.js

---
+++ methods/methodsUseCases.js
@@ -0,0 +1,37 @@
+const cities = ['Orlando', 'Dubai', 'Edinburgh', 'Chennai', 'Accra', 'Denver', 'Eskisehir', 'Medellin', 'Yokohama'];
+
+const nums = [1, 50, 75, 200, 350, 525, 1000];
+
+//  Choose a method that will return undefined
+cities.forEach(city => console.log('Have you visited ' + city + '?'));
+
+// Choose a method that will return a new array
+const longCities = cities.filter(city => city.length > 7);
+
+// Choose a method that will return a single value
+const word = cities.reduce((acc, currVal) => {
+  return acc + currVal[0]
+}, "C");
+
+console.log(word)
+
+// Choose a method that will return a new array
+const smallerNums = nums.map(num => num - 5);
+
+// Choose a method that will return a boolean value
+nums.some(num => num < 0);
+
+
+/*
+Prints:
+Have you visited Orlando?
+Have you visited Dubai?
+Have you visited Edinburgh?
+Have you visited Chennai?
+Have you visited Accra?
+Have you visited Denver?
+Have you visited Eskisehir?
+Have you visited Medellin?
+Have you visited Yokohama?
+CODECADEMY
+*/
