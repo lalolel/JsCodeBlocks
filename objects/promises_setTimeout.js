8d1d75629452389a1db379e42bfb0652c6348beb
Neptune <114935993+lalolel@users.noreply.github.com>
2024-09-17 02:15:07 +0300

    Create promises_setTimeout.js

---
+++ objects/promises_setTimeout.js
@@ -0,0 +1,17 @@
+console.log("This is the first line of code in app.js.");
+// Keep the line above as the first line of code
+// Write your code here:
+
+
+const usingSTO = () => {
+  console.log("This line of code will log to the console last.");
+};
+
+setTimeout(usingSTO, 100);
+
+
+
+
+// Keep the line below as the last line of code:
+console.log("This is the last line of code in app.js.");
+
