5591b20176037a1a08a63efa69b6d36ecb10a6c2
Neptune <114935993+lalolel@users.noreply.github.com>
2024-09-17 01:13:51 +0300

    Create utilities.js

---
+++ classes/explorer/utilities.js
@@ -0,0 +1,8 @@
+// modules/utilities.js
+export const formatNumber = number => {
+  let numStr = String(Math.floor(number));
+  for (let i = numStr.length - 3; i > 0; i -= 3) {
+    numStr = numStr.slice(0, i) + ',' + numStr.slice(i);
+  }
+  return numStr;
+}
